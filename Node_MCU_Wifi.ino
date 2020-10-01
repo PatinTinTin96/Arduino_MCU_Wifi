72f58f7ce30914f6e296542edd8d5e5a620dd78a
= <=>
2020-10-01 16:51:25 -0300

    V1 Codigo arduino

---
+++ Node_MCU_Wifi.ino
@@ -0,0 +1,58 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#ifdef ESP32
+#pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
+#error Select ESP8266 board.
+#endif
+
+ESP8266WebServer server(80); // 80 is the port number
+
+const char* ssid = "VTR-4382214";
+const char* password = "ft8Phhrrw8mx";
+
+
+String ledon,ledoff,led1on,led1off;
+
+void Redon()
+{
+  digitalWrite(4, HIGH);
+  server.send(200, "text/html", ledon);
+}
+
+void Redoff()
+{
+  digitalWrite(4, LOW);
+  server.send(200, "text/html", ledoff);
+}
+
+
+void setup() {
+
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)delay(500);
+
+  Serial.print(WiFi.localIP());
+
+  server.on("/led1on", Redon);
+  server.on("/led1off", Redoff);
+
+
+  server.begin();
+
+
+  pinMode(4, OUTPUT);//D2
+
+
+  digitalWrite(4, LOW);
+
+}
+
+void loop()
+{
+  server.handleClient();
+  delay(1);
+}
